e91b92cf91dc80b01b53ed81ab8f938c529a5119
Michael Shkarubski <40541820+epamekids@users.noreply.github.com>
2019-04-11 19:25:37 +0300

    test commit

--- screens/ResultsScreen.js
+++ screens/ResultsScreen.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import { BarChart } from 'react-native-chart-kit';
-import { Header, Left, Body, Button, Title, Text } from 'native-base';
+import { StyleSheet, View, Text } from 'react-native';
+import { LinearGradient } from 'expo';
 
 export default class Results extends React.Component {
 
@@ -13,57 +11,16 @@ export default class Results extends React.Component {
 
     render() {
         return (
-            <View style={styles.container}>
-                <Header style={{backgroundColor: "#000"}}>
-                    <Left>
-                        <Button transparent>
-                            <Ionicons name='md-arrow-back' style={{color: "#fff", fontSize: 25}} />
-                        </Button>
-                    </Left>
-                    <Body>
-                        <Title>Results</Title>
-                    </Body>
-                </Header>
-                <View style={{backgroundColor: '#fff', flex: 2, alignItems: 'center'}}>
-                    <View style={{marginTop: 20}}>
-                        <BarChart
-                            data={data}
-                            width={270}
-                            height={188}
-                            chartConfig = {{
-                                backgroundGradientFrom: '#fff',
-                                backgroundGradientTo: '#fff',
-                                color: (opacity = 1) => `rgba(0, 158, 92, ${opacity})`,
-                                strokeWidth: 1,
-                            }}
-                        />
+            <LinearGradient
+              colors={colorSuccess}
+              style={{alignItems: 'center', flex: 1}}
+            >
+                <View style={styles.resultsCard}>
+                    <View style={styles.averageCircle}>
+                        <Text style={styles.average}>1 min 08 sec</Text>
                     </View>
                 </View>
-                <View style={{backgroundColor: '#fff', flex: 3, alignItems: 'center'}}>
-                    <View style={styles.forResults}>
-                        <View style={{flexDirection: 'row', marginTop: 20}}>
-                            <Text style={{fontSize: 18, color: "#000"}}>1st Referee</Text>
-                            <Text style={{marginLeft: 55, fontSize: 18, color: "#2ECC98"}}>1 min 9 sec</Text>
-                        </View>
-                        <View style={{height: 1, width: 270, backgroundColor: "#DCDCDC", marginTop: 21}}></View>
-
-                        <View style={{flexDirection: 'row', marginTop: 22}}>
-                            <Text style={{fontSize: 18, color: "#000"}}>2nd Referee</Text>
-                            <Text style={{marginLeft: 55, fontSize: 18, color: "#2ECC98"}}>1 min 12 sec</Text>
-                        </View>
-                        <View style={{height: 1, width: 270, backgroundColor: "#DCDCDC", marginTop: 21}}></View>
-
-                        <View style={{flexDirection: 'row', marginTop: 22}}>
-                            <Text style={{fontSize: 18, color: "#000"}}>3rd Referee</Text>
-                            <Text style={{marginLeft: 55, fontSize: 18, color: "#2ECC98"}}>1 min 06 sec</Text>
-                        </View>
-                        <View style={{height: 1, width: 270, backgroundColor: "#DCDCDC", marginTop: 21}}></View>
-
-                        <Text style={{color:"#000", fontSize: 18, marginTop: 12}}>Average</Text>
-                        <Text style={{color:"#2ECC98", fontSize: 18, marginTop: 7}}>1 min 09 sec</Text>
-                    </View>
-                </View>
-            </View>
+            </LinearGradient>
         );
     }
 }
@@ -75,24 +32,40 @@ let time2 = 66;
 //time1, time2, time3 - показатели трех судей. Могут быть взяты из this.props
 
 const styles = StyleSheet.create({
-    container: {
-        marginTop: 30,
-        backgroundColor: '#fff',
-        flex: 1
+    resultsCard: {
+      flex: 3,
+      width: 300,
+      height: 380,
+      backgroundColor: "#fff",
+      alignItems: 'center',
+      borderWidth: 1,
+      borderRadius: 18,
+      borderColor: "#E1E1E1",
+      shadowColor: "#000",
+      shadowOffset: {width: 0, height: 3},
+      shadowOpacity: 0.27,
+      shadowRadius: 4.65,
+      elevation: 6,
     },
 
-    forResults: {
-      marginTop: 20,
-      width: 260,
-      height: 296,
-      alignItems: 'center',
+    averageCircle: {
+      justifyContent: 'center',
       textAlign: 'center',
+      alignItems: 'center',
+      marginTop: 20,
+      width: 240,
+      height: 240,
+      backgroundColor: "#fff",
+      borderRadius: 120,
+      borderWidth: 2,
+      borderColor: "#2ECC71",
+    },
+
+    average: {
+      fontSize: 33,
+      color: "#2ECC71",
     }
 });
 
-const data = {
-    labels: ['1 Referee', '2 Referee', '3 Referee'],
-    datasets: [{
-        data: [ time1, time2, time3 ]
-    }]
-};
+const colorSuccess = ["#2ECC71", "#1ABC9C"];
+const colorFailure = ["#E74C3C", "#B53471"];
